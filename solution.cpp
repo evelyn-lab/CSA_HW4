66211c32cb3bea50e8a54c6975dc9e7f90282820
evelyn-lab <86932751+evelyn-lab@users.noreply.github.com>
2022-12-16 23:44:24 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,19 +3,19 @@
 #include <vector>
 #include <pthread.h>
 
-std::mutex mtx;
-static bool is_file_input = false;
-static std::vector<int> nums_taken;
-static int counter = 0;
+std::mutex mtx; // синхропримитив
+static bool is_file_input = false; // должен ли ввод осуществляться из файла
+static std::vector<int> nums_taken; // "занятые" номера билетов
+static int counter = 0; // для вывода порядкового номера студента  
 static FILE *file_out = fopen("output.txt", "w");
-static int count_of_students = 0;
+static int count_of_students = 0; // количество потоков (студентов)
 
 void getMark(int id) {
     std::string marks[] = {"A","A+","A-","B","B+", "B-","C",
-                           "C+", "C-","D", "D+", "D-", "F", "F+", "F-"};
-    int random = rand() % 15;
-    std::string mark = marks[random];
-    mtx.lock();
+                           "C+", "C-","D", "D+", "D-", "F", "F+", "F-"}; // массив оценок
+    int random = rand() % 15; // рандом на элемент массива 
+    std::string mark = marks[random]; // выбор оценки с учетом рандома 
+    mtx.lock(); 
     std::cout << "student #"  << id << " turned in the question paper "<< std::endl;
     std::string outline = "student #"  + std::to_string(id) + " turned in the question paper " +'\n';
     fprintf(file_out, "%s", outline.c_str());
@@ -27,22 +27,20 @@ void getMark(int id) {
     mtx.unlock();
 }
 void finishWork(int id) {
-    mtx.lock();
     std::cout << "student #" << id << " finished the question paper "<< std::endl;
     std::cout << std::endl;
     std::string outline = "student #"  + std::to_string(id) + " finished the question paper " +'\n';
     fprintf(file_out, "%s", outline.c_str());
-    mtx.unlock();
     getMark(id);
 }
 
 void *studentWorks(void *vargp) {
     mtx.lock();
     int id = ++counter;
-    int number;
+    int number; // номер билета 
     do {
-        number = 1 + (rand() % count_of_students);
-    } while (std::count(nums_taken.begin(), nums_taken.end(), number));
+        number = 1 + (rand() % count_of_students); // генератор номера билета  
+    } while (std::count(nums_taken.begin(), nums_taken.end(), number)); // проверка номера
     nums_taken.push_back(number);
     std::cout << "student #"  << id << " got question paper "<< number << std::endl;
     std::string outline = "student #" + std::to_string(id) + " got question paper " + std::to_string(number) + '\n';
@@ -70,7 +68,7 @@ int main(int argc, char *argv[]) {
         } else {
             do {
                 std::cout << "please enter count of students in range [2;15]" << std::endl << "> ";
-                std::cin >> count_of_students;                                                   
+                std::cin >> count_of_students;                                                   // Count of bees.
             } while (count_of_students < 2 || count_of_students > 15);
             std::cout << std::endl;
         }
@@ -85,12 +83,12 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     std::cout << std::endl;
-    pthread_t students[count_of_students];
+    pthread_t students[count_of_students]; // массив потоков
     for(size_t i = 0; i < count_of_students; i++) {
-        pthread_create(&students[i], NULL, studentWorks, (void *)&students[i]);
+        pthread_create(&students[i], NULL, studentWorks, (void *)&students[i]); // вызов функции для каждого потока
     }
    for (size_t i = 0; i < count_of_students; i++) {
-        pthread_join(students[i], NULL);
+        pthread_join(students[i], NULL); 
     }
     return 0;
 }
